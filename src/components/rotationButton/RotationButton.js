b619147ff5a70134b9de15d61100ae4235878d22
park cho hye <chohye@172.20.nate.com>
2023-09-10 17:38:30 +0900

    탭 로고 수정 및 버튼 폰트 수정

--- src/components/rotationButton/RotationButton.js
+++ src/components/rotationButton/RotationButton.js
@@ -1,11 +1,11 @@
-import './rotationButton.scss';
+import styles from './rotationButton.module.scss';
 
 const RotationButton = () => {
   return (
     <div class='c-ip-circle-text'>
       <svg
         viewBox='0 0 140 140'
-        className='c-ip-circle-text__svg'
+        className={styles.cIpCircleText__svg}
         style={{
           width: '100px',
           height: '100px',
@@ -17,10 +17,7 @@ const RotationButton = () => {
             d=' M 70, 70 m -59.8, 0 a 59.8,59.8 0 1,1 119.6,0 a 59.8,59.8 0 1,1 -119.6,0'
           ></path>
         </defs>
-        <text
-          className='c-ip-circle-text__title'
-          style={{ fontSize: '18', fill: 'black' }}
-        >
+        <text className={`c-ip-circle-text__title ${styles.buttonText}`}>
           <textPath xlinkHref='#circle-f0ef3aa'>
             Next ・ Next ・ Next ・ Next ・ Next ・ Next ・ Next ・
           </textPath>
